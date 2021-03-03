41d36bce3062984c9b194bd5bd73862de8f4883a
Collin Chase <collinc20@gmail.com>
2021-03-02 21:07:10 -0500

    coalesce
    coalesce

---
+++ random.py
@@ -0,0 +1,23 @@
+# Initialize the netmask and calculate based on CIDR mask
+
+
+
+
+def get_netmask_from_cidr(cidr):
+    mask = [0, 0, 0, 0]
+    for i in range(cidr):
+        mask[i / 8] = mask[i / 8] + (1 << (7 - i % 8))
+    netmask = "{}.{}.{}.{}".format(mask[0], mask[1], mask[2], mask[3])
+    return netmask
+
+
+def get_cidr_from_netmask(netmask):
+    # For later use
+    return (sum([bin(int(bits)).count("1") for bits in netmask.split(".")]))
+
+
+
+
+print(get_cidr_from_netmask("255.0.0.0"))
+
+print(get_netmask_from_cidr(8))
